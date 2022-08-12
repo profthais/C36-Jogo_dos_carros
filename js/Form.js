6cbc3f418ffbaccc24d8828ef85c8b58aca88c1c
thaisfalcao <101199332+thaisfalcao@users.noreply.github.com>
2022-08-12 00:26:32 -0300

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,32 @@
+class Form {
+  constructor() {
+    //Construir o formulário em HTML
+
+  }
+
+  setElementsPosition() {
+    //Definir a posição dos elementos do formulário
+
+  }
+
+  setElementsStyle() {
+    //Adicionar estilo aos elementos do formulário (style.css)
+
+  }
+
+  hide() {
+    //Esconder os elementos quando o jogo iniciar
+
+  }
+
+  //Função para quando apertar o botão de PLAY
+
+
+  display() {
+    //Exibir os elementos nas suas posições e estilos
+
+
+    //Chamar a função de apertar o botão PLAY
+
+  }
+}
